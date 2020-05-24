36e2d1afbbaec2b5140797ee3532ea7bebe6e8b2
sotkou <aa36sk@gmail.com>
2020-05-24 10:42:55 +0300

    προσθήκη οθονης login στο ανοιγμα της εφαρμογης.

---
+++ app/src/main/java/com/example/musicplayer/data/LoginDataSource.java
@@ -0,0 +1,28 @@
+package com.example.musicplayer.data;
+
+import com.example.musicplayer.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(), username); // Διασύνδεση με την βάση δεδομένων για το user name και password
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
